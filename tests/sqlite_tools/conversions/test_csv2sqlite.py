8012299cd2b358ec6c213f40d917a7c9cfeab729
Ryan Richter <ryanrichter028@gmail.com>
2023-10-17 22:46:24 -0400

    implemented a csv (or other _sv) converter to sqlite

---
+++ tests/sqlite_tools/conversions/test_csv2sqlite.py
@@ -0,0 +1,19 @@
+from utils.sqlite_tools.conversions.csv2sqlite import *
+import pytest
+
+params_test_sql_create_table_statement_from_header = [
+    ("test1", ["col1", "col2", "col3"], "CREATE TABLE test1(col1, col2, col3)")
+]
+
+@pytest.mark.parametrize('name,header,expected', params_test_sql_create_table_statement_from_header)
+def test_sql_create_table_statement_from_header(name, header, expected):
+    actual = sql_create_table_statement_from_header(name, header)
+    
+    assert actual == expected
+
+params_test_csv2sqlite = [
+    ['data/examples/test1.csv', None, None]
+]
+@pytest.mark.parametrize('csv_path, sep, db_path', params_test_sql_create_table_statement_from_header)
+def test_csv2sqlite(csv_path, sep, db_path):
+    csv2sqlite(csv_path, sep, db_path)
---
+++ utils/sqlite_tools/conversions/csv2sqlite.py
@@ -0,0 +1,42 @@
+import sqlite3
+import pandas as pd
+# from utils.sqlite_tools.conversions.csv2sqlite import csv2sqlite
+
+def sql_create_table_statement_from_header(name: str, header: list[str]) -> str:
+    columns = ", ".join(header)
+    return f"CREATE TABLE {name}({columns})"
+
+separators = {
+    "csv": ',',
+    "tsv": '\t',
+    "psv": '|'
+}
+def csv2sqlite(csv_path: str, separator=None, db_path=None) -> None:
+    no_extension = "".join(csv_path.split('.')[:-1])
+    name = no_extension.split("/")[-1]
+    if separator is None:
+        extension = csv_path.split('.')[-1]
+        separator = separators[extension]
+    if db_path is None:
+        # replace the file extension and save in the same place
+        db_path = "".join(csv_path.split('.')[:-1]) + ".db"
+    # load the csv
+    df = pd.read_csv(csv_path)
+    # make a connection
+    db = sqlite3.connect(db_path)
+    # make a cursor
+    cur = db.cursor()
+    # insert all of the data appropriately
+    cur.execute(sql_create_table_statement_from_header(name, df.columns))
+    # write the database
+    # statement = f"""INSERT INTO {name} VALUES({(("?, "*len(df.values[0]))[:-2])})"""
+    # cur.executemany(statement, df.values)
+    # print(df)
+    for row in df.values:
+        data = ",".join([str(x) for x in row])
+        statement = f"""INSERT INTO {name} VALUES({data})"""
+        cur.execute(statement)
+        
+    db.commit()
+    db.close()
+    return db_path
