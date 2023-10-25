312bfb3dedb5d63b97508da360e795406d3a6620
Ryan Richter <ryanrichter028@gmail.com>
2023-10-24 20:49:30 -0400

    added purge_directory function

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,28 @@
 from flask import Flask
+import os
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+# helper functions -----------------------------------------------------------
+def purge_directory(path: str):
+    files = os.listdir(path)
+    for file in files:
+        file = os.path.join(path, file)
+        if os.path.isfile(file):
+            os.remove(file)
+        elif os.path.isdir(file):
+            purge_directory(file)
+            os.removedirs(file)
+        else:
+            raise RuntimeError(f"{file} is neither a directory nor a file")
+    
+
+# app start/stop -------------------------------------------------------------
+def shutdown(app: Flask) -> None:
+    # make sure there are no temporary files
+    purge_directory(config["TMP"])
+
 
 def init_app() -> Flask:
     """Initialize the core application"""
